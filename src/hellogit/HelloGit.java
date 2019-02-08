4e0b5e9a75322a00fd87739ea646a584741f0b7a
Mohamed El.Bagoury <Mohamed El.Bagoury@DESKTOP-FRP11KU>
2019-02-08 23:21:38 +0200

    Enhancement: adding sayHi()fn creating new class Welcome Class

--- src/hellogit/HelloGit.java
+++ src/hellogit/HelloGit.java
@@ -8,9 +8,13 @@ public class HelloGit {
 
     public static void main(String[] args) {
            showMessage();
+           sayHi();
     }
     public static void showMessage(){
         System.out.println("hello git");
     
+    }
+    public static void sayHi() {
+        System.out.println("Hi...");
     }
 }
---
+++ src/hellogit/Welcome.java
@@ -0,0 +1,11 @@
+
+package hellogit;
+
+
+public class Welcome {
+
+    public static void welcomeMessage(){
+        System.out.println("welcome");
+    }    
+}
+
